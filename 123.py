e6fb97bdb8692ff5a64d6b08640b41999c46115c
tonyhaha833 <168516169+tonyhaha833@users.noreply.github.com>
2024-06-04 20:47:01 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,7 +1,12 @@
 # 載入必要模組
 import os
+# os.chdir(r'C:\Users\user\Dropbox\系務\專題實作\112\金融看板\for students')
+#import haohaninfo
+#from order_Lo8 import Record
 import numpy as np
-import indicator_f_Lo2_short, datetime, indicator_forKBar_short
+#from talib.abstract import SMA,EMA, WMA, RSI, BBANDS, MACD
+#import sys
+import indicator_f_Lo2_short,datetime, indicator_forKBar_short
 import datetime
 import pandas as pd
 import streamlit as st 
@@ -17,48 +22,71 @@ html_temp = """
 		"""
 stc.html(html_temp)
 
+#df = pd.read_excel("kbars_台積電_1100701_1100708_2.xlsx")
+#df = pd.read_excel("kbars_2330_2022-07-01-2022-07-31.xlsx")
+
 # ## 讀取 excel 檔
-#df_original = pd.read_excel('testdata.xlsx')
+# df_original = pd.read_excel("kbars_2330_2022-01-01-2022-11-18.xlsx")
 
 # ## 保存为Pickle文件:
-#df_original.to_pickle('testdata.pkl')
+# df_original.to_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 
-@st.cache_data(ttl=3600, show_spinner="正在加載資料...")
+## 读取Pickle文件
+@st.cache_data(ttl=3600,show_spinner="正在加載資料")
 def load_data(url):
     df = pd.read_pickle(url)
     return df
+df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 
-## 读取Pickle文件
-df_original = load_data('testdata.pkl')
+#df.columns  ## Index(['Unnamed: 0', 'time', 'open', 'low', 'high', 'close', 'volume','amount'], dtype='object')
+df_original = df_original.drop('Unnamed: 0',axis=1)
+#df.columns  ## Index(['time', 'open', 'low', 'high', 'close', 'volume', 'amount'], dtype='object')
+#df['time']
+#type(df['time'])  ## pandas.core.series.Series
+#df['time'][11]
+#df.head()
+#df.tail()
+#type(df['time'][0])
 
 
-
-
-st.subheader("選擇開始與結束的日期, 區間:2019-01-02 至 2024-04-30")
-start_date = st.text_input('選擇開始日期 (日期格式: 2019-01-02)', '2019-01-02')
-end_date = st.text_input('選擇結束日期 (日期格式: 2024-04-30)', '2019-04-30')
+##### 選擇資料區間
+st.subheader("選擇開始與結束的日期, 區間:2022-01-03 至 2022-11-18")
+start_date = st.text_input('選擇開始日期 (日期格式: 2022-01-03)', '2022-01-03')
+end_date = st.text_input('選擇結束日期 (日期格式: 2022-11-18)', '2022-11-18')
 start_date = datetime.datetime.strptime(start_date,'%Y-%m-%d')
 end_date = datetime.datetime.strptime(end_date,'%Y-%m-%d')
-df_original['time'] = pd.to_datetime(df_original['time'])
-st.write("df_original['time'] 列的类型:", df_original['time'].dtype)
+# 使用条件筛选选择时间区间的数据
 df = df_original[(df_original['time'] >= start_date) & (df_original['time'] <= end_date)]
-
 
 
 ###### (2) 轉化為字典 ######:
 KBar_dic = df.to_dict()
-
+#type(KBar_dic)
+#KBar_dic.keys()  ## dict_keys(['time', 'open', 'low', 'high', 'close', 'volume', 'amount'])
+#KBar_dic['open']
+#type(KBar_dic['open'])  ## dict
+#KBar_dic['open'].values()
+#type(KBar_dic['open'].values())  ## dict_values
 KBar_open_list = list(KBar_dic['open'].values())
 KBar_dic['open']=np.array(KBar_open_list)
-
+#type(KBar_dic['open'])  ## numpy.ndarray
+#KBar_dic['open'].shape  ## (1596,)
+#KBar_dic['open'].size   ##  1596
 
 KBar_dic['product'] = np.repeat('tsmc', KBar_dic['open'].size)
+#KBar_dic['product'].size   ## 1596
+#KBar_dic['product'][0]      ## 'tsmc'
 
 KBar_time_list = list(KBar_dic['time'].values())
 KBar_time_list = [i.to_pydatetime() for i in KBar_time_list] ## Timestamp to datetime
 KBar_dic['time']=np.array(KBar_time_list)
 
-
+# KBar_time_list[0]        ## Timestamp('2022-07-01 09:01:00')
+# type(KBar_time_list[0])  ## pandas._libs.tslibs.timestamps.Timestamp
+#KBar_time_list[0].to_pydatetime() ## datetime.datetime(2022, 7, 1, 9, 1)
+#KBar_time_list[0].to_numpy()      ## numpy.datetime64('2022-07-01T09:01:00.000000000')
+#KBar_dic['time']=np.array(KBar_time_list)
+#KBar_dic['time'][80]   ## Timestamp('2022-09-01 23:02:00')
 
 KBar_low_list = list(KBar_dic['low'].values())
 KBar_dic['low']=np.array(KBar_low_list)
@@ -77,15 +105,41 @@ KBar_dic['amount']=np.array(KBar_amount_list)
 
 
 ######  (3) 改變 KBar 時間長度 (以下)  ########
+# Product_array = np.array([])
+# Time_array = np.array([])
+# Open_array = np.array([])
+# High_array = np.array([])
+# Low_array = np.array([])
+# Close_array = np.array([])
+# Volume_array = np.array([])
 
 Date = start_date.strftime("%Y-%m-%d")
 
-st.subheader("設定一根 K 棒的時間長度(日)")
-cycle_duration = st.number_input('輸入一根 K 棒的時間長度(單位:日)', key="KBar_duration")
+st.subheader("設定一根 K 棒的時間長度(分鐘)")
+import streamlit as st
+
+# 让用户选择时间单位的选项
+time_units = ["分鐘", "小時", "天"]
+selected_unit = st.selectbox("选择时间单位", time_units)
+
+# 根据用户选择的时间单位进行转换
+if selected_unit == "分钟":
+    unit_conversion = 1
+elif selected_unit == "小时":
+    unit_conversion = 60
+else:
+    unit_conversion = 1440
+
+# 获取用户输入的 K 棒时间长度
+cycle_duration = st.number_input('输入一根 K 棒的时间长度（单位：{}）'.format(selected_unit), value=1, key="KBar_duration") * unit_conversion
 cycle_duration = int(cycle_duration)
+#cycle_duration = 1440   ## 可以改成你想要的 KBar 週期
+#KBar = indicator_f_Lo2.KBar(Date,'time',2)
+KBar = indicator_forKBar_short.KBar(Date,cycle_duration)    ## 設定cycle_duration可以改成你想要的 KBar 週期
 
-KBar = indicator_forKBar_short.KBar(Date,cycle_duration * 1440)    ## 設定cycle_duration可以改成你想要的 KBar 週期
-
+#KBar_dic['amount'].shape   ##(5585,)
+#KBar_dic['amount'].size    ##5585
+#KBar_dic['time'].size    ##5585
 
 for i in range(KBar_dic['time'].size):
     
@@ -98,10 +152,35 @@ for i in range(KBar_dic['time'].size):
     high_price= KBar_dic['high'][i]
     qty =  KBar_dic['volume'][i]
     amount = KBar_dic['amount'][i]
-    tag = KBar.AddPrice(time, open_price, close_price, low_price, high_price, qty)
+    #tag=KBar.TimeAdd(time,price,qty,prod)
+    tag=KBar.AddPrice(time, open_price, close_price, low_price, high_price, qty)
+    
+    # 更新K棒才判斷，若要逐筆判斷則 註解下面兩行, 因為計算 MA是利用收盤價, 而在 KBar class 中的 "TimeAdd"函數方法中, 收盤價只是一直附加最新的 price 而已.
+    #if tag != 1:
+        #continue
+    #print(KBar.Time,KBar.GetOpen(),KBar.GetHigh(),KBar.GetLow(),KBar.GetClose(),KBar.GetVolume()) 
+    
+    
+        
+# #type(KBar.Time[1:-1]) ##numpy.ndarray       
+# Time_array =  np.append(Time_array, KBar.Time[1:-1])    
+# Open_array =  np.append(Open_array,KBar.Open[1:-1])
+# High_array =  np.append(High_array,KBar.High[1:-1])
+# Low_array =  np.append(Low_array,KBar.Low[1:-1])
+# Close_array =  np.append(Close_array,KBar.Close[1:-1])
+# Volume_array =  np.append(Volume_array,KBar.Volume[1:-1])
+# Product_array = np.append(Product_array,KBar.Prod[1:-1])
 
 KBar_dic = {}
 
+# ## 形成 KBar 字典:
+# KBar_dic['time'] =  Time_array   
+# KBar_dic['product'] =  Product_array
+# KBar_dic['open'] =  Open_array
+# KBar_dic['high'] =  High_array
+# KBar_dic['low'] =  Low_array
+# KBar_dic['close'] =  Close_array
+# KBar_dic['volume'] =  Volume_array
 
  ## 形成 KBar 字典 (新週期的):
 KBar_dic['time'] =  KBar.TAKBar['time']   
@@ -112,7 +191,13 @@ KBar_dic['high'] =  KBar.TAKBar['high']
 KBar_dic['low'] =  KBar.TAKBar['low']
 KBar_dic['close'] =  KBar.TAKBar['close']
 KBar_dic['volume'] =  KBar.TAKBar['volume']
-#
+# KBar_dic['time'].shape  ## (2814,)
+# KBar_dic['open'].shape  ## (2814,)
+# KBar_dic['high'].shape  ## (2814,)
+# KBar_dic['low'].shape  ## (2814,)
+# KBar_dic['close'].shape  ## (2814,)
+# KBar_dic['volume'].shape  ## (2814,)
+#KBar_dic['time'][536]
 ######  改變 KBar 時間長度 (以上)  ########
 
 
@@ -140,6 +225,8 @@ KBar_df['MA_short'] = KBar_df['close'].rolling(window=ShortMAPeriod).mean()
 #### 尋找最後 NAN值的位置
 last_nan_index_MA = KBar_df['MA_long'][::-1].index[KBar_df['MA_long'][::-1].apply(pd.isna)][0]
 
+
+
 #####  (ii) RSI 策略   #####
 #### 順勢策略
 ### 設定長短 RSI 的 K棒 長度:
@@ -165,6 +252,22 @@ KBar_df['RSI_Middle']=np.array([50]*len(KBar_dic['time']))
 
 ### 尋找最後 NAN值的位置
 last_nan_index_RSI = KBar_df['RSI_long'][::-1].index[KBar_df['RSI_long'][::-1].apply(pd.isna)][0]
+
+
+# #### 逆勢策略
+# ### 建立部位管理物件
+# OrderRecord=Record() 
+# ### 計算 RSI指標, 天花板與地板
+# RSIPeriod=5
+# Ceil=80
+# Floor=20
+# MoveStopLoss=30
+# KBar_dic['RSI']=RSI(KBar_dic,timeperiod=RSIPeriod)
+# KBar_dic['Ceil']=np.array([Ceil]*len(KBar_dic['time']))
+# KBar_dic['Floor']=np.array([Floor]*len(KBar_dic['time']))
+
+# ### 將K線 Dictionary 轉換成 Dataframe
+# KBar_RSI_df=pd.DataFrame(KBar_dic)
 
 
 ###### (5) 將 Dataframe 欄位名稱轉換  ###### 
@@ -202,7 +305,7 @@ with st.expander("K線圖, 移動平均線"):
 
 
 ##### K線圖, RSI
-with st.expander("K線圖, 長短 RSI"):
+with st.expander("長短 RSI"):
     fig2 = make_subplots(specs=[[{"secondary_y": True}]])
     #### include candlestick with rangeselector
     fig2.add_trace(go.Candlestick(x=KBar_df['Time'],
@@ -217,3 +320,98 @@ with st.expander("K線圖, 長短 RSI"):
     
     fig2.layout.yaxis2.showgrid=True
     st.plotly_chart(fig2, use_container_width=True)
+
+
+
+
+
+
+print(KBar_df.columns)
+
+
+# 計算KDJ指標
+def calculate_kdj(df, n=9, m1=3, m2=3):
+    df['low_min'] = df['Low'].rolling(window=n, min_periods=1).min()
+    df['high_max'] = df['High'].rolling(window=n, min_periods=1).max()
+    df['rsv'] = (df['Close'] - df['low_min']) / (df['high_max'] - df['low_min']) * 100
+    df['k'] = df['rsv'].ewm(com=m1 - 1, adjust=False).mean()
+    df['d'] = df['k'].ewm(com=m2 - 1, adjust=False).mean()
+    df['j'] = 3 * df['k'] - 2 * df['d']
+    df.drop(['low_min', 'high_max', 'rsv'], axis=1, inplace=True)
+
+# 將KDJ指標添加到KBar DataFrame中
+calculate_kdj(KBar_df)
+
+# 設置實單交易策略
+def trading_strategy(df):
+    df['buy_signal'] = (df['k'] > df['d']) & (df['k'].shift(1) < df['d'].shift(1))
+    df['sell_signal'] = (df['k'] < df['d']) & (df['k'].shift(1) > df['d'].shift(1))
+    # 根據實單交易策略執行相應的交易操作，這裡可以添加您的交易程式碼
+
+# 執行交易策略
+trading_strategy(KBar_df)
+
+# 繪製包含KDJ指標和交易信號的圖表
+with st.expander("K線圖, KDJ指標, 實單交易策略"):
+    fig3 = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    # K線圖
+    fig3.add_trace(go.Candlestick(x=KBar_df['Time'],
+                    open=KBar_df['Open'], high=KBar_df['High'],
+                    low=KBar_df['Low'], close=KBar_df['Close'], name='K線'),
+                   secondary_y=True)
+    
+    # KDJ指標
+    fig3.add_trace(go.Scatter(x=KBar_df['Time'], y=KBar_df['k'], mode='lines',line=dict(color='blue', width=2), name='K'), 
+                  secondary_y=False)
+    fig3.add_trace(go.Scatter(x=KBar_df['Time'], y=KBar_df['d'], mode='lines',line=dict(color='red', width=2), name='D'), 
+                  secondary_y=False)
+    fig3.add_trace(go.Scatter(x=KBar_df['Time'], y=KBar_df['j'], mode='lines',line=dict(color='green', width=2), name='J'), 
+                  secondary_y=False)
+    
+    # 交易信號
+    fig3.add_trace(go.Scatter(x=KBar_df['Time'], y=KBar_df['Close'], mode='markers', marker=dict(color='green', size=8), 
+                               name='買入信號', customdata=KBar_df['buy_signal']), secondary_y=True)
+    fig3.add_trace(go.Scatter(x=KBar_df['Time'], y=KBar_df['Close'], mode='markers', marker=dict(color='red', size=8), 
+                               name='賣出信號', customdata=KBar_df['sell_signal']), secondary_y=True)
+    
+    fig3.layout.yaxis2.showgrid=True
+    st.plotly_chart(fig3, use_container_width=True)
+
+
+
+# 計算布林通道指標
+def calculate_bollinger_bands(df, window=20, num_std_dev=2):
+    df['MA'] = df['Close'].rolling(window=window).mean()
+    df['std_dev'] = df['Close'].rolling(window=window).std()
+    df['upper_band'] = df['MA'] + (num_std_dev * df['std_dev'])
+    df['lower_band'] = df['MA'] - (num_std_dev * df['std_dev'])
+
+# 添加布林通道指標到KBar DataFrame中
+calculate_bollinger_bands(KBar_df)
+
+# 繪製包含布林通道指標的圖表
+with st.expander("K線圖, 布林通道"):
+    fig4 = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    # K線圖
+    fig4.add_trace(go.Candlestick(x=KBar_df['Time'],
+                    open=KBar_df['Open'], high=KBar_df['High'],
+                    low=KBar_df['Low'], close=KBar_df['Close'], name='K線'),
+                   secondary_y=True)
+    
+    # 布林通道
+    fig4.add_trace(go.Scatter(x=KBar_df['Time'], y=KBar_df['upper_band'], mode='lines',line=dict(color='orange', width=2), name='Upper Band'), 
+                  secondary_y=False)
+    fig4.add_trace(go.Scatter(x=KBar_df['Time'], y=KBar_df['MA'], mode='lines',line=dict(color='blue', width=2), name='MA'), 
+                  secondary_y=False)
+    fig4.add_trace(go.Scatter(x=KBar_df['Time'], y=KBar_df['lower_band'], mode='lines',line=dict(color='purple', width=2), name='Lower Band'), 
+                  secondary_y=False)
+    
+    fig4.layout.yaxis2.showgrid=True
+    st.plotly_chart(fig4, use_container_width=True)
+
+
+
+
+
